af56fa50d6ac625bacd24befadbb733372e6944d
anavuongdin <timeskipfor8months@gmail.com>
2022-08-04 09:46:32 +0700

    "Add missing files"

--- manipulathor_plugin/armpointnav_constants.py
+++ manipulathor_plugin/armpointnav_constants.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from constants import ABS_PATH_OF_TOP_LEVEL_DIR
+# from constants import ABS_PATH_OF_TOP_LEVEL_DIR
 
 TRAIN_OBJECTS = ["Apple", "Bread", "Tomato", "Lettuce", "Pot", "Mug"]
 TEST_OBJECTS = ["Potato", "SoapBottle", "Pan", "Egg", "Spatula", "Cup"]
@@ -11,11 +11,11 @@ UNWANTED_MOVE_THR = 0.01
 DISTANCE_EPS = 1e-9
 DISTANCE_MAX = 10.0
 
-dataset_json_file = os.path.join(
-    ABS_PATH_OF_TOP_LEVEL_DIR, "datasets", "apnd-dataset", "starting_pose.json"
-)
-try:
-    with open(dataset_json_file) as f:
-        ARM_START_POSITIONS = json.load(f)
-except Exception:
-    raise Exception("Dataset not found in {}".format(dataset_json_file))
+# dataset_json_file = os.path.join(
+#     ABS_PATH_OF_TOP_LEVEL_DIR, "datasets", "apnd-dataset", "starting_pose.json"
+# )
+# try:
+#     with open(dataset_json_file) as f:
+#         ARM_START_POSITIONS = json.load(f)
+# except Exception:
+#     raise Exception("Dataset not found in {}".format(dataset_json_file))
