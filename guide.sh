1ff99e9f35bcb4d085f0091bef14e940ec5535c5
anavuongdin <timeskipfor8months@gmail.com>
2022-08-04 10:48:02 +0700

    "Revert repo"

--- guide.sh
+++ guide.sh
@@ -23,13 +23,9 @@ pip install -r requirements.txt
 
 # Step 4 - copy libraries to venv
 ## NOTE: may need to modify the destination paths
-#cp -R manipulathor_baselines /media/SSD/anvd4/venv/lib/python3.8/site-packages/allenact
-#cp -R ithor_arm /media/SSD/anvd4/venv/lib/python3.8/site-packages/allenact
-#cp -R manipulathor_utils /media/SSD/anvd4/venv/lib/python3.8/site-packages/allenact
-### Before these command lines, create projects.manipulathor_disturb_free
-cp -R manipulathor_baselines/armpointnav_baselines /media/SSD/anvd4/venv/lib/python3.8/site-packages/allenact/projects/manipulathor_disturb_free
-cp -R manipulathor_plugin /media/SSD/anvd4/venv/lib/python3.8/site-packages/allenact_plugins
-cp -R embodiedai/sensors /media/SSD/anvd4/venv/lib/python3.8/site-packages/allenact/embodiedai
+cp -R manipulathor_baselines /media/SSD/anvd4/venv/lib/python3.8/site-packages/allenact
+cp -R ithor_arm /media/SSD/anvd4/venv/lib/python3.8/site-packages/allenact
+cp -R manipulathor_utils /media/SSD/anvd4/venv/lib/python3.8/site-packages/allenact
 
 # Step 5 - fix paths inside files
 sudo env "PATH=$PATH" python /media/SSD/anvd3/manipulathor/scripts/startx.py
